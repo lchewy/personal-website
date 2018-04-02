a5f926016ed5ac3ac8b778259a1a788218e8eb6a
Leslie Chiu <leslie@Leslies-MacBook-Pro.local>
2018-04-02 16:46:25 -0400

    deploy test

--- js/index.js
+++
@@ -1,8 +0,0 @@
-window.onscroll = ()=>{addStickyClass()};
-
-const navbar = document.getElementById("navbar")
-const sticky = navbar.offsetTop;
-
-const addStickyClass = () =>{
-    window.pageYOffset >= sticky ? navbar.classList.add("sticky") : navbar.classList.remove("sticky");
-}
