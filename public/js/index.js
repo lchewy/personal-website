24299543cea893197961e36610542e76cf8fa7da
Leslie Chiu <leslie@Leslies-MacBook-Pro.local>
2018-04-02 19:26:59 -0400

    overlay on project section

--- public/js/index.js
+++ public/js/index.js
@@ -6,3 +6,7 @@ const sticky = navbar.offsetTop;
 const addStickyClass = () =>{
     window.pageYOffset >= sticky ? navbar.classList.add("sticky") : navbar.classList.remove("sticky");
 }
+
+const smoothScroll = ()=>{
+    document.querySelector('.about').scrollIntoView({ behavior: 'smooth' });
+}
